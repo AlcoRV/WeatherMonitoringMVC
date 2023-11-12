a610eccee49fce091a5fdeff3fbb2e5efe04e0f7
AlcoRV <zennerqwert@ya.ru>
2023-11-12 17:33:58 +0300

    added style to listpager

--- WeatherMonitoringMVC/Controllers/HomeController.cs
+++ WeatherMonitoringMVC/Controllers/HomeController.cs
@@ -3,21 +3,22 @@ using WeatherMonitoringMVC.Models;
 using WeatherMonitoringMVC.Tools;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using X.PagedList;
 
 namespace WeatherMonitoringMVC.Controllers
 {
+    public class WeatherDescriptionViewModel
+    {
+        public IPagedList<WeatherDescription> WeatherDescriptions { get; set; }
+        public int? Month { get; set; }
+        public int? Year { get; set; }
+    }
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IAllWeatherDescription _weatherDescriptions;
-
-        public class WeatherDescriptionViewModel
-        {
-            public IEnumerable<WeatherDescription> WeatherDescriptions { get; set; }
-            public int? Month { get; set; }
-            public int? Year { get; set; }
-        }
 
         public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment, IAllWeatherDescription weatherDescriptions)
         {
@@ -41,10 +42,10 @@ namespace WeatherMonitoringMVC.Controllers
            var weatherDescriptions = _weatherDescriptions.WeatherDescriptions;
             if (month != null && month > 0 && month <= 12) { weatherDescriptions = weatherDescriptions.Where(w => w.Date.Month == month); }
             if (year != null) { weatherDescriptions = weatherDescriptions.Where(w => w.Date.Year == year); }
-
+            
             return View(new WeatherDescriptionViewModel()
             {
-                WeatherDescriptions = weatherDescriptions,
+                WeatherDescriptions = weatherDescriptions.ToPagedList(page ?? 1, 10),
                 Year = year,
                 Month = month
             });
